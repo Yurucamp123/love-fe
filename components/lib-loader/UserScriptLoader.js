8bcfa9bccc63f9de8d64006c81e40369fb9bad14
Binh <binhltse173315@fpt.edu.vn>
2025-05-07 10:12:25 +0700

    update config

--- components/lib-loader/UserScriptLoader.js
+++ components/lib-loader/UserScriptLoader.js
@@ -3,42 +3,44 @@ import { useEffect } from "react";
 
 const UserScriptLoader = () => {
     useEffect(() => {
-        const scripts = [
-            "/assets/lib/user/js/vendor/jquery-3.7.1.min.js",
-            "/assets/lib/user/js/vendor/jquery-migrate-3.3.0.min.js",
-            "/assets/lib/user/js/vendor/bootstrap.bundle.min.js",
+        if (typeof window !== 'undefined') {
+            const scripts = [
+                "assets/lib/user/js/vendor/jquery-3.7.1.min.js",
+                "assets/lib/user/js/vendor/jquery-migrate-3.3.0.min.js",
+                "assets/lib/user/js/vendor/bootstrap.bundle.min.js",
 
-            "/assets/lib/user/js/plugins/magnific-popup.js",
-            "/assets/lib/user/js/plugins/perfect-scrollbar.min.js",
-            "/assets/lib/user/js/plugins/swiper-bundle.min.js",
-            "/assets/lib/user/js/plugins/slick.js",
-            "/assets/lib/user/js/plugins/jquery.carouselTicker.js",
-            "/assets/lib/user/js/plugins/masonry.min.js",
-            "/assets/lib/user/js/plugins/scrollup.js",
-            "/assets/lib/user/js/plugins/wow.js",
-            "/assets/lib/user/js/plugins/waypoints.js",
-            "/assets/lib/user/js/plugins/counterup.js",
-            "/assets/lib/user/js/plugins/bootstrap-datepicker.js",
-            "/assets/lib/user/js/plugins/dark.js",
+                "assets/lib/user/js/plugins/magnific-popup.js",
+                "assets/lib/user/js/plugins/perfect-scrollbar.min.js",
+                "assets/lib/user/js/plugins/swiper-bundle.min.js",
+                "assets/lib/user/js/plugins/slick.js",
+                "assets/lib/user/js/plugins/jquery.carouselTicker.js",
+                "assets/lib/user/js/plugins/masonry.min.js",
+                "assets/lib/user/js/plugins/scrollup.js",
+                "assets/lib/user/js/plugins/wow.js",
+                "assets/lib/user/js/plugins/waypoints.js",
+                "assets/lib/user/js/plugins/counterup.js",
+                "assets/lib/user/js/plugins/bootstrap-datepicker.js",
+                "assets/lib/user/js/plugins/dark.js",
 
-            "/assets/lib/user/js/vendor/jquery.countdown.min.js",
-            "/assets/lib/user/js/plugins/noUISlider.js",
-            "/assets/lib/user/js/plugins/slider.js",
-            
-            "/assets/lib/user/js/main.js?v=1.0.0",
-        ];
+                "assets/lib/user/js/vendor/jquery.countdown.min.js",
+                "assets/lib/user/js/plugins/noUISlider.js",
+                "assets/lib/user/js/plugins/slider.js",
 
-        scripts.forEach((src) => {
-            const script = document.createElement("script");
-            script.src = src;
-            script.defer = true;
-            document.body.appendChild(script);
-        });
+                "assets/lib/user/js/main.js?v=1.0.0",
+            ];
 
-        return () => {
-            const scripts = document.querySelectorAll("script[src]");
-            scripts.forEach((script) => script.remove());
-        };
+            scripts.forEach((src) => {
+                const script = document.createElement("script");
+                script.src = src;
+                script.async = true;
+                document.body.appendChild(script);
+            });
+
+            return () => {
+                const scripts = document.querySelectorAll("script[src]");
+                scripts.forEach((script) => script.remove());
+            };
+        }
     }, []);
 
     return null;
